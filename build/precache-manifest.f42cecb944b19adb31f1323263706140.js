cfea1ee033a83e82fbcef4d081c2aabf6d6a269e
Linh Hoang <467424@student.saxion.nl>
2020-06-28 17:04:04 +0200

    AAA

---
+++ build/precache-manifest.f42cecb944b19adb31f1323263706140.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0accf5b0ace0c592c82b0dd6c7346b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dddfa6dabc2cde162917",
+    "url": "/static/css/2.954ec6e0.chunk.css"
+  },
+  {
+    "revision": "da30a63ee41f938596ea",
+    "url": "/static/css/main.a93344be.chunk.css"
+  },
+  {
+    "revision": "dddfa6dabc2cde162917",
+    "url": "/static/js/2.023d99c5.chunk.js"
+  },
+  {
+    "revision": "ea8c492eefde8c840df2391535ef9c13",
+    "url": "/static/js/2.023d99c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da30a63ee41f938596ea",
+    "url": "/static/js/main.646e74c1.chunk.js"
+  },
+  {
+    "revision": "daa10a90664a83c3b100",
+    "url": "/static/js/runtime-main.f7973f6b.js"
+  },
+  {
+    "revision": "a3026ef0d7ef66b56aebbc75cb8f785a",
+    "url": "/static/media/act_logo.a3026ef0.png"
+  },
+  {
+    "revision": "8548334b5d28d48e377739ec0189e243",
+    "url": "/static/media/login_background.8548334b.jpg"
+  },
+  {
+    "revision": "b67fd4cb02c09bfd91cae8f90e27457d",
+    "url": "/static/media/profile.b67fd4cb.jpg"
+  }
+]);
